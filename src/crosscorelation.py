201e3d4ed6c1d3477b7e84cf8e8af1127883df2e
alexpashevich <alexpashevich@github.com>
2015-12-03 23:40:36 +0100

    Q4 works

--- src/crosscorelation.py
+++ src/crosscorelation.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.signal import correlate, correlate2d
 import matplotlib.pyplot as plt
 import re
+import cv2
 
 
 def create_impulse_signal(N, a):
@@ -14,6 +15,14 @@ def energy(signal):
     e = 0
     for s in signal:
         e += s ** 2
+    return e
+
+
+def energy2D(img):
+    e = 0
+    for x in range(len(img)):
+        for y in range(len(img[0])):
+            e += img[x, y] ** 2
     return e
 
 
@@ -99,8 +108,8 @@ def display_plot(x, y, title="Plot", xlabel="x", ylabel="y", limits=[], legend=F
     plt.show()
 
 
-def display_image(image):
-    plt.imshow(image, plt.cm.gray, vmin=0, vmax=255)
+def display_image(image, vmin=0, vmax=255):
+    plt.imshow(image, plt.cm.gray, vmin=vmin, vmax=vmax)
     # if SAVE_IMAGE:
         # plt.savefig('../output/img.png')
     plt.show()
@@ -137,6 +146,23 @@ def show_complex_plot(ordinates, titles=None, xlabels=None, ylabels=None, ylim=N
     plt.show()
 
 
+def compute_integrate_energy(img):
+    # in energy_sqr[x, y] we store sum of img[x', y']^2 for every x' <= x and y' <= y
+    energy_sqr = np.zeros(img.shape)
+    energy_sqr[0, 0] = img[0, 0] ** 2
+    for x in range(1, len(energy_sqr)):
+        energy_sqr[x, 0] = img[x, 0] ** 2 + energy_sqr[x - 1, 0]
+    for y in range(1, len(energy_sqr[0])):
+        energy_sqr[0, y] = img[0, y] ** 2 + energy_sqr[0, y - 1]
+    for x in range(len(energy_sqr)):
+        for y in range(len(energy_sqr[0])):
+            energy_sqr[x, y] = img[x, y] ** 2 + energy_sqr[x, y - 1] + energy_sqr[x - 1, y] - energy_sqr[x - 1, y - 1]
+    return energy_sqr
+
+def get_energy(energy_sqr, x, y, w, h):
+    return energy_sqr[x + w, y + h] - energy_sqr[x, y + h] - energy_sqr[x + w, y] + energy_sqr[x, y]
+
+
 if __name__ == '__main__':
     print("Question 2")
 # Questions 2.1 and 2.2
@@ -145,11 +171,11 @@ if __name__ == '__main__':
     subx = correlate(x, sT, mode = 'valid')
     print("Maximum correlation in subvector of x with length " + str(len(subx))) # shit here
     print(subx)
-    show_complex_plot([x, sT, subx],
-                       titles=["Impulse signal of length 100 and peak on 50", "Impulse signal of length 40 and peak on 20", "Correlation"],
-                       xlabels=["k", "k", "k"],
-                       ylabels=["x[k]", "sT[k]", "C[k]"],
-                       ylim=[-0.1, 1.1])
+    # show_complex_plot([x, sT, subx],
+    #                    titles=["Impulse signal of length 100 and peak on 50", "Impulse signal of length 40 and peak on 20", "Correlation"],
+    #                    xlabels=["k", "k", "k"],
+    #                    ylabels=["x[k]", "sT[k]", "C[k]"],
+    #                    ylim=[-0.1, 1.1])
 
 # Question 2.3
     print("E(x) = " + str(energy(x)))
@@ -166,28 +192,56 @@ if __name__ == '__main__':
     x = create_x(20, 60, 200, sigma)
     period = create_one_period_sin_signal(20)
     C = correlate(x, period, mode = "valid")
-    show_complex_plot([x, period, C],
-                      titles=["Noisy signal", "One period sinus signal", "Correlation of period and x"],
-                      xlabels=["k", "k", "k"],
-                      ylabels=["x[k]", "period[k]", "C[k]"])
+    # show_complex_plot([x, period, C],
+    #                   titles=["Noisy signal", "One period sinus signal", "Correlation of period and x"],
+    #                   xlabels=["k", "k", "k"],
+    #                   ylabels=["x[k]", "period[k]", "C[k]"])
 
 # Question 3.5
     M = correlate(x / np.sqrt(energy(x)), period / np.sqrt(energy(period)), "valid")
     print("C* = max(M(x_l, sT)) = " + str(np.max(M)))
     print("index of C* = argmax(M(x_l, sT)) = " + str(np.argmax(M)))
 
-    print("Question 4 - from here code is not good now")
+    print("Question 4")
 # Question 4.1
     img = read_pgm("../input/FindWaldo1.pgm")
     pattern = read_pgm("../input/WadoTarget1.pgm")
-    # display_image(img)
-    # display_image(pattern)
-    corr_2d = correlate2d(img, pattern, mode="same")
-    # display_image(corr_2d)
-    print(np.max(corr_2d), np.argmax(corr_2d))
-    # [y,x] = np.unravel_index(np.argmax(map), corr_2d.map)
-    # print(y)
-    # print(x)
+    img_copy = img * 1. # to make it float
+    pattern_copy = pattern * 1. # to make it float
+    display_image(img)
+    display_image(pattern)
+    corr_2d = correlate2d(img_copy, pattern_copy, mode="valid")
+    display_image(corr_2d, vmin=min(corr_2d.flatten()), vmax=max(corr_2d.flatten()))
+    print("Max of cross correlation = " + str(np.max(corr_2d)))
+    print("Min of cross correlation = " + str(np.min(corr_2d)))
+    [y,x] = np.unravel_index(np.argmax(corr_2d), corr_2d.shape)
+    print("Waldo found at (" + str(x) + ", " + str(y) + ") - incorrect result as we used unnormalized correlation")
+
+# now we want to use normalization
+    energy_pattern = np.sqrt(energy2D(pattern))
+    energy_sqr = compute_integrate_energy(img)
+
+# normalize the corr_2d
+# this probably should be in the report
+    for x in range(len(corr_2d) - 1):
+        for y in range(len(corr_2d[0]) - 1):
+            corr_2d[x, y] /= (np.sqrt(get_energy(energy_sqr, x, y, len(pattern), len(pattern[0]))) * energy_pattern)
+
+# this shouldn't be in report :) it's just one last index cutting which was not normalized as corr_2d has size len(img) - len(pattern) + 1
+    corr_2d = corr_2d[:-1, :-1]
+    display_image(corr_2d, vmin=min(corr_2d.flatten()), vmax=max(corr_2d.flatten()))
+    print("Max of cross correlation = " + str(np.max(corr_2d)))
+    print("Min of cross correlation = " + str(np.min(corr_2d)))
+    [y,x] = np.unravel_index(np.argmax(corr_2d), corr_2d.shape)
+    print("Waldo found at (" + str(x) + ", " + str(y) + ") - right answer")
+
+
+
+
+
+
+
+
 
 
 
